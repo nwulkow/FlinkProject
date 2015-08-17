3f36fc2e5a22c588536f5b599c42f6eae060ccaa
nwulkow <niklas.wulkow@ewetel.net>
2015-08-17 14:11:48 +0200

    Update Pipeline.scala

--- Pipeline.scala
+++ Pipeline.scala
@@ -1,250 +1,238 @@
-package com.github.projectflink.als
+package de.fuberlin.de.largedataanalysis
 
-import java.io.FileWriter
+import java.io.{File, FileWriter}
 
-import com.github.projectflink.common.als.{ALSUtils, Factors, Rating}
-import com.github.projectflink.util.FlinkTools
-import de.fuberlin.de.largedataanalysis.Pipeline
-import org.apache.flink.api.scala._
-import org.apache.flink.util.Collector
-import org.jblas.{Solve, SimpleBlas, FloatMatrix}
+import org.apache.flink.api.scala.{DataSet, ExecutionEnvironment, _}
+import org.apache.flink.ml.MLUtils
+import org.apache.flink.ml.classification.SVM
+import org.apache.flink.ml.common.LabeledVector
+import org.apache.flink.ml.math.DenseVector
+import com.github.projectflink.als.ALSJoin
+
+import scala.reflect.io.Path
+import scala.io.Source
+
+object Pipeline {
 
 
-class ALSJoin(factors: Int, lambda: Double, iterations: Int, seed: Long, persistencePath:
-Option[String]) extends ALSFlinkAlgorithm with Serializable {
+  val env = ExecutionEnvironment.getExecutionEnvironment
+  var matrixaslist = List[List[Double]]()
 
-  def factorize(ratings: DS[RatingType]): Factorization = {
-    null
-  }
+  def main(args: Array[String]) {
 
-  def factorize(ratings: DS[RatingType], ratings2: DS[RatingType],
-                ratings3: DS[RatingType], ratings4: DS[RatingType]): Factorization = {
+    // -------------------------------------------------------
+    // Parameter einlesen
+    if (!parseParameters(args)) {
+      return
+    }
+    // -------------------------------------------------------
 
-    val transposedRatings = ratings2 map { x => Rating(x.item, x.user, x
-      .rating)
+    //Pfade für temporäre Dateien
+    val outputpath: Path = Path(path + "/Output")
+    outputpath.createDirectory()
+
+
+    val outputPath_SVM = path + "/Output/svmvectors"
+    val pw = new FileWriter(new File(outputPath_SVM))
+
+    // PFAD zu den Dateien, (also nicht zu einem einzelnen File!)
+    val path_healthy = path + "/Healthy"
+    val path_diseased = path + "/Diseased"
+
+
+    // Pfade und FileWriter für einige temporäre Dateien:
+    // SVM-File, Netzwerkmatrix,Knoten des Netzwerkes, unvollständige Matrix,
+    val outputPath_Matrix: String = path + "/Output/matrix"
+    val nodes_file_path = path + "/Output/PRBNodes"
+    val incompletematrix_path = path + "/Output/matrix_tocomplete"
+    val resultlabels_path = path + "/Output/labels"
+
+    val network_matrix_healthy_path = path + "/Output/network_matrix_healthy"
+    val network_matrix_diseased_path = path + "/Output/network_matrix_diseased"
+    val network_nodes_healthy_path = path + "/Output/network_nodes_healthy"
+    val network_nodes_diseased_path = path + "/Output/network_nodes_diseased"
+    val network_gdf_healthy = path + "/Output/gdf_healthy"
+    val network_gdf_diseased = path + "/Output/gdf_diseased"
+
+    val pw_incompletematrix = new FileWriter(new File(incompletematrix_path))
+    val pw_resultlabels = new FileWriter(new File(resultlabels_path))
+
+    // -------------------------------------------------------
+
+    // Vorbereitung der Matrix Completion. Reihen sind die verschiedenen Personen, Spalten die Genecounts
+    val numberHealthy = Tools.getListOfFiles(path_healthy).length
+    val numberDiseased = Tools.getListOfFiles(path_diseased).length
+    val matrixAndGenes = PreprocessingMethods.matrixCreation(Array[String](path_healthy, path_diseased), pw_incompletematrix, excludesGenes)
+    var incompleteMatrix = matrixAndGenes._1
+    val allGenes = matrixAndGenes._2
+
+    // -------------------------------------------------------
+
+    incompleteMatrix = ALSJoin.doMatrixCompletion(incompletematrix_path, 10, 100, 42, Some("dummy string"), path, numberHealthy + numberDiseased, allGenes.length)
+
+    // Die Zeilen der vervollständigten Matrix umwandeln in 'LabeledVectors'
+    matrixaslist = PreprocessingMethods.preprocessdataFromMatrix(incompleteMatrix, numberHealthy, pw, matrixaslist)
+
+    // -------------------------------------------------------
+    // -------------------------------------------------------
+    // -------------------------------------------------------
+
+
+    // SVM
+    val svmData: DataSet[LabeledVector] = MLUtils.readLibSVM(env, outputPath_SVM)
+
+    val svm = SVM()
+      .setBlocks(env.getParallelism)
+      .setIterations(100)
+      .setRegularization(0.001)
+      .setStepsize(0.1)
+      .setSeed(42)
+
+    svm.fit(svmData)
+    val f = svm.weightsOption
+    val weights = f.get.collect()
+    val weightsList = weights.toList
+
+    // -------------------------------------------------------
+    // Testen des classifiers
+
+    val files = Tools.getListOfFiles(testFiles_path)
+    val scalarproducts = new Array[Double](files.length)
+    for (k <- Range(0, files.length)) {
+
+      val current_testperson = testFiles_path + "/" + files(k).getName
+      val testperson_data = PreprocessingMethods.readOnePersonsData(current_testperson, excludesGenes)
+      val testperson_counts = testperson_data._1
+      val testperson_genes = testperson_data._2
+
+      // Vervollständigen, wenn GeneCounts bei einer Testperson fehlen. Hier wird natürlich der Wert 0 angenommen
+      val extended_testperson_counts = Tools.mergeCountLists(testperson_counts, testperson_genes, allGenes)
+
+      // Skalarprodukt des classifiers ('weightList(0)') mit den Werten einer Testperson
+      val product = (weightsList(0).toArray, extended_testperson_counts).zipped.map((c1, c2) => c1 * c2).sum
+      scalarproducts(k) = product
+
+      // Erstelltes Label (Vorzeichen des Skalarproduktes) in eine .txt-File schreiben
+      var HealthyOrDiseased = "Healthy"
+      if (product < 0) HealthyOrDiseased = "Diseased"
+      pw_resultlabels.write(HealthyOrDiseased + " : " + files(k).getName + ", " + product)
+      pw_resultlabels.write("\n")
+    }
+    pw_resultlabels.close()
+
+    for (s <- scalarproducts) {
+      println("scalarproduct = " + s.toString)
     }
 
-    val initialItemMatrix = {
-      val itemIDs = ratings.map { x => Tuple1(x.item)} distinct
+    // -------------------------------------------------------
 
-      val initialItemMatrix = generateRandomMatrix(itemIDs map {
-        _._1
-      }, factors, seed)
+    // Die wichtigesten Gene ausgeben
+    val numberOfTopGenes = 10
+    // Höchste Gewichte
+    val sortedweights = weightsList(0).toArray.sorted
+    var topPositiveWeights = List[(String, Double)]()
+    var topNegativeWeights = List[(String, Double)]()
 
-      persistencePath match {
-        case Some(path) =>
-          FlinkTools.persist(initialItemMatrix, path + "initialItemMatrix")
-        case None => (initialItemMatrix)
+    for (k <- Range(0, numberOfTopGenes)) {
+      var correspondingGeneIndex = weightsList(0).toArray.indexOf(sortedweights(k))
+      topNegativeWeights ::=(allGenes(correspondingGeneIndex), sortedweights(k))
+      correspondingGeneIndex = weightsList(0).toArray.indexOf(sortedweights(allGenes.length - 1 - k))
+      topPositiveWeights ::=(allGenes(correspondingGeneIndex), sortedweights(allGenes.length - 1 - k))
+    }
+
+    println("top negative weights : " + topNegativeWeights.reverse)
+    println("top positive weights : " + topPositiveWeights.reverse)
+
+
+
+    // -------------------------------------------------------
+    // -------------------------------------------------------
+    // -------------------------------------------------------
+
+
+    // Netzwerk Vorbereitung:
+    // Korrelationswerte berechnen und schreiben
+
+    var (matrixaslistHealthy, matrixaslistDiseased) = matrixaslist.splitAt(numberHealthy)
+    doNetworkAnalysisAndGDFFIle(matrixaslistHealthy, network_matrix_healthy_path, network_nodes_healthy_path, network_gdf_healthy)
+    doNetworkAnalysisAndGDFFIle(matrixaslistDiseased, network_matrix_diseased_path, network_nodes_diseased_path, network_gdf_diseased)
+
+    def doNetworkAnalysisAndGDFFIle(matrixaslist: List[List[Double]], path_matrix: String, path_nodes: String, gdf_path: String) {
+
+      var meansList = List[Double]()
+      var varianceList = List[Double]()
+      val noGenes = matrixaslist(0).length
+      var matrixaslistTranspose = matrixaslist.transpose
+      val noPeople = matrixaslistTranspose(0).length
+
+      val pw_matrix = new FileWriter(path_matrix)
+      val pw_nodes_file = new FileWriter(path_nodes)
+
+      var network = List[Array[Double]]() // Netzwerk wird gespeichert, um später ein GDF-File zu erzeugen
+
+      // Netzwerk-Matrix wird erzeugt und in ein .txt-File geschrieben. Das Netzwerk ist ungewichtet, der threshold muss angegeben werden
+      for (j <- Range(0, noGenes)) {
+        meansList = StatisticsMethods.meanOfArray(matrixaslistTranspose(j)) :: meansList
+        varianceList = StatisticsMethods.variance(matrixaslistTranspose(j)) :: varianceList
       }
-    }
+      val threshold = 0.8
+      for (j <- Range(0, noGenes)) {
+        pw_nodes_file.write(j.toString)
+        pw_nodes_file.write("\n")
 
-    val iMatrix = initialItemMatrix.iterate(iterations){
-      itemMatrix => {
-        val userMatrix = updateMatrix(ratings4, itemMatrix, lambda)
-        updateMatrix(transposedRatings, userMatrix, lambda)
-      }
-    }
-
-    val itemMatrix = persistencePath match {
-      case Some(path) =>
-        FlinkTools.persist(iMatrix, path + "items")
-      case None =>
-        iMatrix
-    }
-
-    val userMatrix = updateMatrix(ratings3, itemMatrix, lambda)
-    Factorization(userMatrix, itemMatrix)
-  }
-
-  def updateMatrix(ratings: DataSet[RatingType], items: DataSet[FactorType],
-                   lambda: Double): DataSet[FactorType] = {
-    val uVA = items.join(ratings).where(0).equalTo(1) {
-      (item, ratingEntry) => {
-        val Rating(uID, _, rating) = ratingEntry
-
-        (uID, rating, item.factors)
-      }
-    }
-
-    uVA.groupBy(0).reduceGroup{
-      (vectors, col: Collector[FactorType]) => {
-
-        var uID = -1
-        val triangleSize = (factors*factors - factors)/2 + factors
-        val xtx = FloatMatrix.zeros(triangleSize)
-
-        val vector = FloatMatrix.zeros(factors)
-        var n = 0
-
-        for((id, rating, vectorData) <- vectors){
-          uID = id
-
-          val v = new FloatMatrix(vectorData)
-
-          SimpleBlas.axpy(rating, v, vector)
-          ALSUtils.outerProductInPlace(v, xtx, factors)
-
-          n += 1
-        }
-
-        val fullMatrix = FloatMatrix.zeros(factors, factors)
-
-        ALSUtils.generateFullMatrix(xtx, fullMatrix, factors)
-
-        var counter = 0
-
-        while(counter < factors){
-          fullMatrix.data(counter*factors + counter) += lambda.asInstanceOf[ElementType] * n
-          counter += 1
-        }
-
-        col.collect(new Factors(uID, Solve.solvePositive(fullMatrix, vector).data))
-      }
-    }//.withConstantSet("0")
-  }
-}
-
-object ALSJoin extends ALSFlinkRunner with ALSFlinkToyRatings {
-
-
-  def main(args: Array[String]): Unit = {
-    //val inputRatings = "/home/mi/nwulkow/ADL/Projekt/Data/miRNASeq/BCGSC__IlluminaHiSeq_miRNASeq/Level_3/Test/testmatrix"
-    //val outputPath = "\"/home/mi/nwulkow/ADL/Projekt/Data/miRNASeq/BCGSC__IlluminaHiSeq_miRNASeq/Level_3/Test"
-    //doMatrixCompletion(inputRatings, 4, 100, 42, Some("dummy"), outputPath, 3, 3)
-
-    parseCL(args) map {
-      config => {
-        import config._
-
-        val env = ExecutionEnvironment.getExecutionEnvironment
-        val ratings = readRatings(inputRatings, env)
-        val ratings2 = readRatings(inputRatings, env)
-        val ratings3 = readRatings(inputRatings, env)
-        val ratings4 = readRatings(inputRatings, env)
-
-        val als = new ALSJoin(factors, 0.1, iterations, seed, persistencePath)
-        val factorization = als.factorize(ratings, ratings2, ratings3, ratings4)
-
-        outputFactorization(factorization, outputPath)
-
-        env.execute("ALS benchmark")
-
-
-        val output_user_path = outputPath + "/userFactorsFile"
-        val output_item_path = outputPath + "/itemFactorsFile"
-        var itemmatrix = Array.ofDim[Double](3,factors)
-        var usermatrix = Array.ofDim[Double](3,factors)
-
-        for(j <- Range(1,5)) {
-          var items = scala.io.Source.fromFile(output_item_path + "/" + j.toString).getLines()
-          var users = scala.io.Source.fromFile(output_user_path + "/" + j.toString).getLines()
-
-          for (l1 <- items){
-            val l1sep = l1.split(",")
-            val row_item = l1sep(0).filterNot("(".toSet).toInt - 1
-            for (i <- Range(0, factors)) {
-              itemmatrix(row_item)(i) = l1sep(i + 1).filterNot(")".toSet).filterNot("(".toSet).toDouble
-            }
+        for (h <- Range(j, noGenes)) {
+          if (threshold < Math.abs(StatisticsMethods.correlationcoefficient(matrixaslistTranspose(j), matrixaslistTranspose(h)))) {
+            pw_matrix.write(j.toString + " " + h.toString)
+            pw_matrix.write("\n")
+            network = Array[Double](j, h) :: network
           }
-          for (u1 <- users){
-            val u1sep = u1.split(",")
-            val row_user = u1sep(0).filterNot("(".toSet).toInt - 1
-            for (i <- Range(0, factors)) {
-              usermatrix(row_user)(i) = u1sep(i + 1).filterNot(")".toSet).filterNot("(".toSet).toDouble
-            }
-          }
         }
-
-
-
-        val resultmatrix = mult(usermatrix,itemmatrix)
-
-        printMatrix(itemmatrix)
-        printMatrix(usermatrix)
-        printMatrix(resultmatrix)
-
-
       }
-    } getOrElse{
-      println("Could not parse command line parameters.")
+      // FileWriter schließen
+      pw_matrix.close()
+      pw_nodes_file.close()
+      val pageranks = PageRankBasicForPipeline.executePRB(nodes_file_path, outputPath_Matrix, "", matrixaslistTranspose(0).length)
+      val pageranks_list = pageranks.collect().toList
+
+      // Ein GDF-File schreiben mit dem Netzwerk und den PageRankBasic-Werten
+      StatisticsMethods.writeGDFFile(network, pageranks, allGenes, gdf_path)
+
+
+    }
+
+  }
+
+
+
+  // -------------------------------------------------------
+
+
+  private var path = ""
+  private var excludesGenes: Array[String] = null
+  private var testFiles_path = ""
+  private def parseParameters(args: Array[String]): Boolean = {
+    if (args.length > 0) {
+      path = args(0)
+      excludesGenes = args(1).split(",")
+      testFiles_path = args(2)
+      true
+    } else {
+      false
     }
   }
 
 
-  def doMatrixCompletion(inputRatings: String, factors: Int, iterations: Int,
-                         seed: Long, persistencePath: Option[String], outputPath: String, norows : Int, nocols: Int): Array[Array[Double]] = {
 
 
-
-    val env = ExecutionEnvironment.getExecutionEnvironment
-    val ratings = readRatings(inputRatings, env)
-    val ratings2 = readRatings(inputRatings, env)
-    val ratings3 = readRatings(inputRatings, env)
-    val ratings4 = readRatings(inputRatings, env)
-    val als = new ALSJoin(factors, 0.1, iterations, seed, persistencePath)
-    val factorization = als.factorize(ratings, ratings2, ratings3, ratings4)
-
-    outputFactorization(factorization, outputPath)
-    env.execute("ALS benchmark")
-
-
-    val output_user_path = outputPath + "/userFactorsFile"
-    val output_item_path = outputPath + "/itemFactorsFile"
-    var itemmatrix = Array.ofDim[Double](norows,factors)
-    var usermatrix = Array.ofDim[Double](nocols,factors)
-
-    for(j <- Range(1,5)) {
-      var items = scala.io.Source.fromFile(output_item_path + "/" + j.toString).getLines()
-      var users = scala.io.Source.fromFile(output_user_path + "/" + j.toString).getLines()
-
-      for (l1 <- items){
-        val l1sep = l1.split(",")
-        val row_item = l1sep(0).filterNot("(".toSet).toInt - 1
-        for (i <- Range(0, factors)) {
-          itemmatrix(row_item)(i) = l1sep(i + 1).filterNot(")".toSet).filterNot("(".toSet).toDouble
-        }
-      }
-      for (u1 <- users){
-        val u1sep = u1.split(",")
-        val row_user = u1sep(0).filterNot("(".toSet).toInt - 1
-        for (i <- Range(0, factors)) {
-          usermatrix(row_user)(i) = u1sep(i + 1).filterNot(")".toSet).filterNot("(".toSet).toDouble
-        }
-      }
-    }
-
-
-
-    val resultmatrix = mult(usermatrix,itemmatrix)
-
-    //printMatrix(itemmatrix)
-    //printMatrix(usermatrix)
-    //printMatrix(resultmatrix)
-    val file = "/home/mi/nwulkow/ADL/Projekt/Data/miRNASeq/BCGSC__IlluminaHiSeq_miRNASeq/Level_3/completed"
-    Pipeline.writeMatrixToCSV(resultmatrix, new FileWriter((file)))
-    return resultmatrix
-
-    //return Array[Array[Double]]()
+  def readmiRNA(env: ExecutionEnvironment, path: String, columns: Array[Int]): DataSet[GeneData] = {
+    env.readCsvFile[GeneData](
+      path,
+      fieldDelimiter = "\t",
+      includedFields = columns)
   }
 
 
 
-  def printMatrix(matrix: Array[Array[Double]]){
-    for (i <- Range(0,matrix.length)) {
-      for (j <- Range(0, matrix(0).length)) {
-
-        print(matrix(i)(j) + "  ")
-      }
-      println()
-    }
-  }
-
-
-  // Kopiert von http://rosettacode.org/wiki/Matrix_multiplication
-  def mult[A](a: Array[Array[Double]], b: Array[Array[Double]]): Array[Array[Double]] = {
-    for (row <- a)
-      yield for(col <- b)
-        yield row zip col map Function.tupled(_*_) reduceLeft (_+_)
-  }
+  // -------------------------------------------------------
 
 }
